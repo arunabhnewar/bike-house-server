e71b4fcf69cce46e339280acfeb73773dafa7692
arunabhnewar <newardevil@gmail.com>
2021-11-11 00:07:59 +0600

    second commit

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ async function run() {
         await client.connect();
         const database = client.db('bike-house-database');
         const productCollection = database.collection('products');
+        const purchaseCollection = database.collection('purchases');
+        const userCollection = database.collection('users');
 
         // GET API
         app.get('/products', async (req, res) => {
@@ -59,6 +61,71 @@ async function run() {
             const result = await productCollection.insertOne(req.body);
             res.send(result);
         })
+
+        // Add Purchase API
+        app.post('/purchases', async (req, res) => {
+            const purchase = req.body;
+            const result = await purchaseCollection.insertOne(purchase);
+            res.send(result);
+        })
+        // GET Purchase API
+        app.get('/purchases', async (req, res) => {
+            const cursor = purchaseCollection.find({});
+            const purchase = await cursor.toArray();
+            console.log(purchase);
+            res.send(purchase);
+        })
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        // Add Users
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.json(result)
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        // GET EMAIL API
+        app.get('/purchase/:email', async (req, res) => {
+            const email = req.params.email;
+            const result = await purchaseCollection.find({ email }).toArray();
+            res.json(result);
+        })
+
+        app.put('users/admin', async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await userCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await userCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            } else {
+                res.status(403).json({ message: 'you do not have access to make admin' })
+            }
+        })
+
     }
     finally {
         // await client.close();
