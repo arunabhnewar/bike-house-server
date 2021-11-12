0ead3babfac931fbc5d23ece1ad8b4fee533aa83
arunabhnewar <newardevil@gmail.com>
2021-11-12 15:08:07 +0600

    fifth commit

--- index.js
+++ index.js
@@ -129,6 +129,14 @@ async function run() {
             res.send(result);
         })
 
+        // DELETE Products API
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
         // await client.close();
